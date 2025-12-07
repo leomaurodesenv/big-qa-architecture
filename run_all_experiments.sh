48da6478b89c4548fcbd4bbda3aebada40c17888
leomaurodesenv <leo.mauro.desenv@gmail.com>
2025-12-06 22:17:48 -0300

    feat: update experiments

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -7,7 +7,7 @@ mkdir -p out
 
 # Define datasets and models
 DATASETS=("DISASTER_TWEET_JAILBREAKING" "AEGIS" "TRUST_AI_RLAB_JAILBREAK")
-MODELS=("ARCH_GUARD" "LLAMA_GUARD" "SAMSUNG_JAILBREAK_FILTER" "SHIELD_GEMMA" "CHAIN")
+MODELS=("LLAMA_GUARD" "SAMSUNG_JAILBREAK_FILTER" "SHIELD_GEMMA" "DEBERTA" "BERT" "ELECTRA")
 
 # Function to convert enum name to filename (e.g., ARCH_GUARD -> arch_guard)
 to_filename() {
